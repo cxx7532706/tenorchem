225dda511da3260e5998a6fca216b8b40bedfb55
Xuanxi Chen <charles@Xuanxis-MacBook-Pro.local>
2017-06-20 12:16:46 +1000

    Migrations commit

---
+++ Migrations/20170619125753_postgres.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using tenorchem.Models;
+
+namespace tenorchem.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20170619125753_postgres")]
+    partial class postgres
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn)
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("tenorchem.Models.Product", b =>
+                {
+                    b.Property<int>("ProductId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Comment");
+
+                    b.Property<string>("Name")
+                        .IsRequired();
+
+                    b.Property<string>("Purity");
+
+                    b.HasKey("ProductId");
+
+                    b.ToTable("Products");
+                });
+
+            modelBuilder.Entity("tenorchem.Models.PurchaseRecord", b =>
+                {
+                    b.Property<int>("PurchaseRecordId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<double>("PaidBackPerTon");
+
+                    b.Property<double>("PricePerTon");
+
+                    b.Property<int>("ProductId");
+
+                    b.Property<DateTime>("PurchaseDate");
+
+                    b.Property<double>("Quantity");
+
+                    b.Property<int>("SupplierId");
+
+                    b.Property<double>("TotalPaidBack");
+
+                    b.Property<double>("TotalPaidPrice");
+
+                    b.Property<string>("comment");
+
+                    b.HasKey("PurchaseRecordId");
+
+                    b.HasIndex("ProductId");
+
+                    b.HasIndex("SupplierId");
+
+                    b.ToTable("PurchaseRecords");
+                });
+
+            modelBuilder.Entity("tenorchem.Models.Supplier", b =>
+                {
+                    b.Property<int>("SupplierId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Address");
+
+                    b.Property<string>("Comment");
+
+                    b.Property<string>("ContactNumber");
+
+                    b.Property<string>("Contactor");
+
+                    b.Property<string>("SupplierName")
+                        .IsRequired();
+
+                    b.HasKey("SupplierId");
+
+                    b.ToTable("Suppliers");
+                });
+
+            modelBuilder.Entity("tenorchem.Models.User", b =>
+                {
+                    b.Property<int>("id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("isAdmin");
+
+                    b.Property<string>("passWord")
+                        .IsRequired();
+
+                    b.Property<string>("userName")
+                        .IsRequired();
+
+                    b.HasKey("id");
+
+                    b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("tenorchem.Models.PurchaseRecord", b =>
+                {
+                    b.HasOne("tenorchem.Models.Product", "Product")
+                        .WithMany("PurchaseRecord")
+                        .HasForeignKey("ProductId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("tenorchem.Models.Supplier", "Supplier")
+                        .WithMany("PurchaseRecord")
+                        .HasForeignKey("SupplierId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ Migrations/20170619125753_postgres.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace tenorchem.Migrations
+{
+    public partial class postgres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SupplierId",
+                table: "Suppliers",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PurchaseRecordId",
+                table: "PurchaseRecords",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SupplierId",
+                table: "Suppliers",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PurchaseRecordId",
+                table: "PurchaseRecords",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+        }
+    }
+}
